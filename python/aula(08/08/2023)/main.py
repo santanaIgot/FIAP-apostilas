00dfcd9a84391d4d0b315a0aa473304b6b85906a
santanaIgot <santanaigoor01@gmail.com>
2023-08-08 22:30:48 -0300

    chave-valor em python

---
+++ python/aula(08/08/2023)/main.py
@@ -0,0 +1,17 @@
+lista = ["igor", "negao"]
+# lista é uma estrutura de dados posicional 
+
+# lista abrimos com colchetes, dicionario com chave
+
+# chave-valor
+dicionario = {"95003":{"igor","analise e dev sistemas", "manhã"}}
+
+print(dicionario["95003"])
+
+# para adicionar um elemento precisa passar5 chave e dic
+chave = input("entre com a chave:")
+dicionario[chave] = {"nome":"melin", "curso":"ana e dev sistemas ","horario": "noite"}
+
+print(dicionario["95003"]["curso"])
+
+
